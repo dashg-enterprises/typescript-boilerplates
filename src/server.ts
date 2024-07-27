1c88d0033cb9fde516d295bbbf25fbde3a4ae705
chadgarlandscg <chadgarlandscg@gmail.com>
2024-07-27 17:15:05 -0600

    Revert to health check and turn off CD in preparation for Terraform

--- src/server.ts
+++ src/server.ts
@@ -7,37 +7,37 @@ import loadContainer from "./inversify.config";
 import { DomainError } from "./application/models/DomainError";
 import { getSecret } from "./infrastructure/getSecret";
 
-getSecret().then(secret => console.log("Wrapped up", secret.username, secret.password));
+// getSecret().then(secret => console.log("Wrapped up", secret.username, secret.password));
 
-// loadContainer().then(container => {
-//     const inversifyWrapper = new InversifyExpressServer(container);
-//     inversifyWrapper.setConfig(app => {
-//         app.use(express.json());
-//         app.use(cors({
-//             origin: "http://localhost:1234"
-//         }));
-//     });
+loadContainer().then(container => {
+    const inversifyWrapper = new InversifyExpressServer(container);
+    inversifyWrapper.setConfig(app => {
+        app.use(express.json());
+        app.use(cors({
+            origin: "http://localhost:1234"
+        }));
+    });
 
-//     inversifyWrapper.setErrorConfig(app => {
-//         app.use((error: DomainError, req, res, next) => {
-//             if (error.type == "DomainError") {
-//                 return res.status(409).json({
-//                     code: "409 Conflict",
-//                     type: error.type,
-//                     resource: req.params.id,
-//                     message: error.message
-//                 });
-//             }
-//             return res.status(500).json({
-//                 code: "500 Server Error",
-//                 message: error.message
-//             });
-//         });
-//     })
+    inversifyWrapper.setErrorConfig(app => {
+        app.use((error: DomainError, req, res, next) => {
+            if (error.type == "DomainError") {
+                return res.status(409).json({
+                    code: "409 Conflict",
+                    type: error.type,
+                    resource: req.params.id,
+                    message: error.message
+                });
+            }
+            return res.status(500).json({
+                code: "500 Server Error",
+                message: error.message
+            });
+        });
+    })
 
-//     const app = inversifyWrapper.build();
+    const app = inversifyWrapper.build();
 
-//     app.listen(3000, () => {
-//         console.log("The server is up and running!");
-//     });
-// });
+    app.listen(3000, () => {
+        console.log("The server is up and running!");
+    });
+});
